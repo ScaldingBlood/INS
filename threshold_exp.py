90b00cae41a8d5a50df11c8e9ae775825978b9f0
scaldingblood@gmail.com <scaldingblood@gmail.com>
2019-01-04 18:26:25 +0800

    little bug about exp fixed

--- threshold_exp.py
+++ threshold_exp.py
@@ -12,7 +12,6 @@ class ThresholdEXP:
     mag_gyo_list = []
     mag_res_to_judge = []
 
-
     def add_avg_acc(self, acc):
         self.avg_acc_list.append(acc)
 
@@ -26,7 +25,7 @@ class ThresholdEXP:
         self.res_to_judge.append(res)
 
     def add_avg_gyo(self, gyo):
-        self.res_to_judge.append(gyo)
+        self.avg_gyo_list.append(gyo)
 
     def add_mag_gyo(self, gyo):
         self.mag_gyo_list.append(gyo)
@@ -37,10 +36,10 @@ class ThresholdEXP:
     def show(self):
         plt.figure()
         t = np.arange(0, len(self.res_to_judge) * 0.01, 0.01)
-        plt.plot(t, self.sum_delta, 'r--', t, self.sum_gryo, 'g--', t, self.res_to_judge, 'bo')
+        plt.plot(t, self.sum_delta, 'r--', t, self.sum_gryo, 'g--', t, self.res_to_judge, 'b--')
         plt.figure(2)
         mag_t = np.arange(0, len(self.mag_res_to_judge) * 0.01, 0.01)
-        plt.plot(mag_t, self.avg_gyo_list, 'r--', t, self.mag_gyo_list, 'g--', t, self.mag_res_to_judge, 'bo')
+        plt.plot(mag_t, self.avg_gyo_list, 'r--', mag_t, self.mag_gyo_list, 'g--', mag_t, self.mag_res_to_judge, 'b--')
         plt.show()
         # for i in range(len(self.res_to_judge)):
         #     print(str(self.sum_delta[i]) + " " + str(self.sum_gryo[i]) + " " + str(self.res_to_judge[i]) + " " + str(self.res_to_judge[i] < 0.3))
